794707a9d2520bef2c193c761de5f359db301748
Lukas Mahler <lukas.mah@outlook.at>
2021-04-17 18:49:21 +0200

    added TODOs for search

--- www/search.js
+++ www/search.js
@@ -1,3 +1,20 @@
+/*
+TODO: use query parameter q to store search term
+
+TODO: Use MET API to search
+
+TODO: load highlights if no searchterm was given (highlights.json)
+TODO: Consider only artworks that have images
+TODO: Return only the first 100 per search
+TODO: Link the artwork image of each result to the corresponding Framing page
+TODO: Reaplace 'search our collection of...' with actual search term 'Searching for "<term>"'
+        when the search is done: 'Found <number> artworks for "<term>"'
+        watch out for plural (artwork vs artworks)
+
+
+
+*/
+
 const form = document.querySelector('form.search-form')
 
 
