9817626762f68893765054b5a54ee66a59d12f20
Lukas Mahler <lukas.mah@outlook.at>
2021-04-17 20:43:03 +0200

    connect search to dom to show testobject

--- www/search.js
+++ www/search.js
@@ -16,10 +16,7 @@ TODO: Reaplace 'search our collection of...' with actual search term 'Searching 
         watch out for plural (artwork vs artworks)
 */
 
-import { artwork } from "./artwork";
-
 const form = document.querySelector('form.search-form');
-
 
 form.addEventListener('submit', event => { 
     event.preventDefault();
@@ -40,37 +37,44 @@ form.addEventListener('submit', event => {
 });
 
 function loadHighlights() {
-    console.log("loadHighlights()");    
+    console.log(`loadHighlights()`);
+
+    const gallery = document.getElementById('gallery');
+    const thumb = new Thumb("testTitle", "testArtist", "2021-05-21", "www.test.com", "test alt");
+    const thumbDiv = createThumbElement(thumb);
+
+    console.log(thumbDiv);
+    gallery.appendChild(thumbDiv);
 };
 
+function createThumbElement(thumb) {
+    console.log(`Creating thumb element: ${JSON.stringify(thumb)}`)
+    
+    const div = document.createElement('div');
+    div.classList.add("thumb");
+    
+    // TODO: set correct link for href to framing.html
+    div.innerHTML = 
+    `<a href="framing.html/${thumb.href}">
+    <img src="${thumb.url}" alt="${thumb.alt}">
+    <div class="museum-label">
+    <span class="artist">${thumb.artist}</span>
+    <span class="title">${thumb.title}</span>
+    <span class="date">${thumb.date}</span>
+    </div>
+    </a>`;
+    
+    return div;
+}
+
+// Container to contain parameters for creating a Thumb
 class Thumb {
-    constructor(title, artist, date, href, url, alt) {
+    constructor(title, artist, date, href, imgUrl, alt) {
         this.title = title;
         this.artist = artist;
         this.date = date;
         this.href = href;
-        this.url = url;
+        this.imgUrl = imgUrl;
         this.alt = alt;
     }
 }
-
-function createThumbElement(thumb) {
-    console.log(`Creating thumb element: ${thumb}`)
-
-    const div = document.createElementlement('div');
-    div.classList.add("thumb");
-
-    // TODO: set correct link for href to framing.html
-    div.innerHTML = 
-        `<a href="framing.html/${thumb.href}">
-        <img src="${thumb.url}" alt="${thumb.alt}">
-        <div class="museum-label">
-            <span class="artist">${thumb.artist}</span>
-            <span class="title">${thumb.title}</span>
-            <span class="date">${thumb.date}</span>
-        </div>
-        </a>`;
-
-    return div;
-}
-
