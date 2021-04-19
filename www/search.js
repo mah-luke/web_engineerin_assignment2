1083d778517f6a729815ee93e4c31ab443622b23
Lukas Mahler <lukas.mah@outlook.at>
2021-04-19 19:13:21 +0200

    fixed typo

--- www/search.js
+++ www/search.js
@@ -27,7 +27,7 @@ const CURRENT_URL = new URL(window.location.href);
 document.addEventListener('DOMContentLoaded', () => {
     console.log("--- load site ---");
     search(CURRENT_URL.searchParams.get('q'));
-    cart.innerHTML = `Cart(${localStorage.getItem('cart')!= null? localStorage.getItem('cart').split('{').length-1 : "" })`;
+    cart.innerHTML = `Cart (${localStorage.getItem('cart')!= null? localStorage.getItem('cart').split('{').length-1 : "" })`;
 });
 
 form.addEventListener('submit', event => {
