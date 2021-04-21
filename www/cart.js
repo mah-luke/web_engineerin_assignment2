6b3a9b0604c5d0e3979378e50ce965f8d9deebbe
Lukas Mahler <lukas.mah@outlook.at>
2021-04-21 20:30:29 +0200

    remove todos and polish code

--- www/cart.js
+++ www/cart.js
@@ -1,18 +1,3 @@
-/*
-
-    Use the local storage key cart to store the shopping cart.
-    You should represent the cart as a JSON array of objects containing an objectID and the framing parameters, but nothing more.
-    In particular, the cart should not contain the artwork metadata or the calculated price of each item.
-    For each item in the cart, show a preview of the framed artwork using the Met API and the helper functions in frame.js. The preview image should link to the corresponding Framing page.
-    For each item in the cart, show the usual metadata (artist, title, date) and a textual description of the configuration. The description should be like "Medium print in a 3.3 cm natural frame with a 1.7 cm mint mat." or "Small print in a 5 cm classic frame." (if the mat has width zero).
-    Show the price of each item, as well as the sum total.
-    Allow the user to remove items from the cart by clicking on the circled "x". Removing an item from the cart should not cause the page to reload.
-    Display the most recently added item on top.
-    If there are no items in the cart, show the message "There are no items in your shopping cart." and nothing else (except for the usual page header).
-
-
-*/
-
 import * as ArtworkCache from  './artwork-cache.js';
 import * as MetApi from './met-api-wrapper.js';
 import * as StorageHandler from './storageHandler.js';
@@ -107,15 +92,12 @@ async function main() {
                 console.warn(event);
                 event.preventDefault();
     
-                //TODO: removing wron child (up / down)
-                // deleting of 2+ elements
-    
                 let urlPic = new URL(event.target.parentElement.previousElementSibling.firstElementChild.href);
                 console.log(urlPic);
                 let objectID = urlPic.searchParams.get('objectID');
                 let cur = StorageHandler.getObject('cart');
                 console.log(cur);
-                cur = cur.filter(cart => cart.objectID != objectID); /// get this objectid
+                cur = cur.filter(cart => cart.objectID != objectID);
                 console.log(cur);
                 
                 StorageHandler.store('cart', cur);
