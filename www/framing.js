ebdc32924092d34473bbc8c30870b5e77e8c21e2
DonikaSkenderbeut <e11929150@student.tuwien.ac.at>
2021-04-27 18:33:20 +0200

    framing.js

--- www/framing.js
+++ www/framing.js
@@ -37,3 +37,63 @@ async function framing() {
      </a>`;     
 
 }
+
+export function update(matColor, frameStyle, printSize, frameWidth, matWidth) {
+    setMatColor(matColor);
+    setFrameStyle(frameStyle);
+    setPrintSize(printSize);
+    document.getElementById("frameValue").value = frameWidth/10;
+    document.getElementById("frameSlider").value = frameWidth/10;
+    document.getElementById("matWidth").value = matWidth/10;
+    document.getElementById("matSlider").value = matWidth/10;
+}
+
+function setMatColor(matColor) {
+
+    const containers = document.querySelectorAll('.mat-color-item');
+    console.log(containers);
+    for (let index = 0; index < containers.length; index++) {
+        if (containers[index].firstElementChild.value === matColor) {
+            containers[index].firstElementChild.checked = true;
+            break;
+
+        }
+
+    }
+
+}
+
+function setFrameStyle(frameStyle) {
+
+    const containers = document.querySelectorAll('.frame-style-item');
+    console.log(containers);
+    for (let index = 0; index < containers.length; index++) {
+
+        if (containers[index].firstElementChild.value === frameStyle) {
+            containers[index].firstElementChild.checked = true;
+            break;
+
+        }
+
+    }
+
+
+}
+
+function setPrintSize(printSize) {
+
+    const small = document.getElementById("print-size-s")
+    const medium = document.getElementById("print-size-m")
+    const large = document.getElementById("print-size-l")
+
+    if (small.value == printSize) {
+        small.checked = true;
+
+    } else if (medium.value == printSize) {
+        medium.checked = true;
+
+    } else {
+        large.checked = true;
+    }
+
+}
